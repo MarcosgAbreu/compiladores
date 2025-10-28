4f97d39428c216f79472041007261c5597298ca3
Marcos Abreu <63922981+MarcosgAbreu@users.noreply.github.com>
2025-10-27 21:32:42 -0300

    att

--- compatibilidade.py
+++ compatibilidade.py
@@ -1,12 +1,27 @@
-v1 = float(input("Digite o primeiro valor: "))
-v2 = float(input("Digite o segundo valor: "))
+def tipo_variavel(entrada):
+    try:
+        valor = float(entrada)
+        if valor % 1 == 0:
+            return "int"
+        else:
+            return "float"
+    except:
+        if len(entrada) == 1:
+            return "char"
+        else:
+            return "string"
 
-v1_int = (v1 % 1 == 0)
-v2_int = (v2 % 1 == 0)
 
-if v1_int and v2_int:
-    print("Compativel")  # dois inteiros
-elif not v1_int and not v2_int:
-    print("Compativel")  # dois floats
+v1 = input("Digite o primeiro valor: ")
+v2 = input("Digite o segundo valor: ")
+
+t1 = tipo_variavel(v1)
+t2 = tipo_variavel(v2)
+
+print(f"Tipo do primeiro valor: {t1}")
+print(f"Tipo do segundo valor: {t2}")
+
+if (t1 == "int" and t2 == "int") or (t1 == "float" and t2 == "float"):
+    print("Compativel")
 else:
-    print("Nao compativel")  # tipos diferentes
+    print("Nao compativel")
